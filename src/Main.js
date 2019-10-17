1e17beac7c8dac92bb61ac6f72f030be9cf50c93
Leonardo Faria <leonardo.faria@b2wdigital.com>
2019-10-17 12:38:36 -0300

    pegando feed do cartao

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 var commandLineArgs = require('command-line-args')
 var readline = require('readline-sync')
+var fs = require('fs')
 var httpService = require('./HttpService')
 var qrCodeService = require('./QRCodeService')
 
@@ -8,19 +9,18 @@ const optionDefinitions = [
     { name: 'password', alias: 'p', type: String }
 ];
 
-async function main (){
-    const options = commandLineArgs(optionDefinitions);
-
+async function login() {
     const discoveryUrls = await httpService.nuDiscovery() 
     const discoveryAPPUrls = await httpService.nuDiscoveryAPP() 
 
     const loginCredentials = await httpService.nuLogin(discoveryUrls.login, options.cpf, options.password)
-    console.log(loginCredentials)
-
+    
     if(loginCredentials.error) {
         console.log("Something goes wrong: " + loginCredentials.error)
         process.exit(1)
     }
+
+    console.log("Logado!")
 
     const qrCodeData = qrCodeService.showQRCode()
 
@@ -29,15 +29,42 @@ async function main (){
     const authenticatedData = await httpService.authenticateWithQrCode(discoveryAPPUrls.lift, qrCodeData, loginCredentials.access_token)
 
     if(authenticatedData.error) {
-        console.log("Something goes wrong: " + authenticatedData.error)
+        console.log("Something goes wrong 2: " + authenticatedData.error)
         process.exit(1)
     }
 
-    console.log("Foi!!")
+    const dataToFile = {
+        accessToken: authenticatedData.access_token,
+        feedUrl: authenticatedData._links.events.href
+    }
 
-    const feed = httpService.getCardFeed(authenticatedData.access_token, authenticatedData._links.events.href )
+    fs.writeFileSync('./.nubankConfigrc', JSON.stringify(dataToFile, null, 2) )
+}
 
-    console.log(feed)
+async function getFeed() {
+    const authenticatedData = JSON.parse(fs.readFileSync('./.nubankConfigrc'))
+    console.log(authenticatedData)
+
+    return await httpService.getCardFeed(authenticatedData.accessToken, authenticatedData.feedUrl )
+}
+
+async function main (){
+    const options = commandLineArgs(optionDefinitions);
+
+    if(!fs.existsSync('./.nubankConfigrc')) {
+        await login()
+    }
+
+    const feed = await getFeed()
+
+    if(feed.error) {
+        console.log("Can't get feed: " + feed.error)
+        console.log("Login maybe expired")
+        login()
+        feed = await getFeed()
+    }
+    
+    console.log(feed.events.find(e => e.id === "5d8cee40-8026-4f6d-a4f9-61965f24c63e" ))
 }
 
 main();
