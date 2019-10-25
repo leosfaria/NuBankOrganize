dfa70740ae19063f8cae41f2cddcfeaca168a489
Matheus Calegaro <matheuscalegaro@outlook.com>
2019-10-25 19:22:38 -0300

    feat: WIP charts

---
+++ front/src/mock/report.js
@@ -0,0 +1,24 @@
+export const report = {
+  "October-2019": {
+    "amount": 41917,
+    "tags": [
+      {
+        "description": "outros",
+        "amount": 38885
+      },
+      {
+        "description": "passagem",
+        "amount": 3032
+      }
+    ]
+  },
+  "September-2019": {
+    "amount": 67977,
+    "tags": [
+      {
+        "description": "outros",
+        "amount": 67977
+      }
+    ]
+  }
+}
